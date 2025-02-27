2034ee26a99f258ccaad1a267f4c6b6ba8ac0b05
bobjoe400 <cnamattern@gmail.com>
2025-02-26 16:25:37 -0800

    fixing sending packet

--- NestUi/Utils/nest_serial.py
+++ NestUi/Utils/nest_serial.py
@@ -1,14 +1,12 @@
 import serial
 from PySide6.QtWidgets import QMessageBox
 
-def send_packet(serial_port, baud_rate, ascii_data):
+def send_packet(serial_port, baud_rate, data):
     try:
         timeout = 2
         # Open the serial connection with the specified port and baud rate
         with serial.Serial(serial_port, baudrate=baud_rate, timeout=timeout) as ser:
-            # Convert the ASCII data to bytes
-            packet = ascii_data.encode('ascii')
-            ser.write(packet)
+            ser.write(data)
             print("Packet sent over serial!")
             QMessageBox.information(None, "Success", "Packet sent successfully!")
             return True
